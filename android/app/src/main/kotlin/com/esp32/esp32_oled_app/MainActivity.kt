4af3a14655da721de1e1367bb0e867b0c006930b
wenhongquan <1432244016@qq.com>
2026-03-30 18:15:55 +0800

    `fix`

--- android/app/src/main/kotlin/com/esp32/esp32_oled_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/esp32/esp32_oled_app/MainActivity.kt
@@ -2,11 +2,9 @@ package com.esp32.esp32_oled_app
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import com.example.esp32_oled_app.WifiScanPlugin
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        WifiScanPlugin().registerWith(flutterEngine, applicationContext)
     }
 }
